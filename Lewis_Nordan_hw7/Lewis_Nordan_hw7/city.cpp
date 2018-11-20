ac27f6e8e63115d8f7e579a999c0ed44606ddd97
bevous <43658573+bevous@users.noreply.github.com>
2018-11-20 13:10:58 -0600

    input is done

--- Lewis_Nordan_hw7/Lewis_Nordan_hw7/city.cpp
+++ Lewis_Nordan_hw7/Lewis_Nordan_hw7/city.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "city.h"
-using namespace Cities;
+using namespace std;
 
 city::city()
 {
@@ -14,7 +14,7 @@ city::~city()
 {
 }
 
-void Cities::city::setCity(string location)
+void  city::setCity(string location)
 {
 	istringstream s(location);
 	string temp;
@@ -26,31 +26,29 @@ void Cities::city::setCity(string location)
 	cordinates[1] = stod(temp);
 }
 
-string Cities::city::getName()
+string  city::getName()
 {
 	return name;
 }
 
-string Cities::city::getCountry()
+string  city::getCountry()
 {
 	return country;
 }
 
-vector<double> Cities::city::getCordinates()
+vector<double>  city::getCordinates()
 {
 	return cordinates;
 }
 
-double Cities::city::distance(city& other)
+double  city::distance(city& other)
 {
+	const double R = 3959.0;
 	double dLat = this->cordinates[0] - other.getCordinates()[0];
 	double Dlong = this->cordinates[1] - other.getCordinates()[1];
-	double a = (sin(dLat / 2)) ^ 2 + cos(this->cordinates[0]) * cos(other.getCordinates()[0]) * (sin(Dlong / 2)) ^ 2;
-	double c;
-	double d;
+	double a = ( pow(sin(dLat / 2),2.0) + cos(this->cordinates[0]) * cos(other.getCordinates()[0]) * pow(sin(Dlong / 2),2));
+	double c = 2 * a * tan(2 * (sqrt(a), sqrt(1 - a)));
+	double d = R * c;
 
-
-
-
-	return 0.0;
+	return d;
 }
--- Lewis_Nordan_hw7/Lewis_Nordan_hw7/city.h
+++ Lewis_Nordan_hw7/Lewis_Nordan_hw7/city.h
@@ -1,35 +1,30 @@
-#ifndef city_
-#define city_
-
 #pragma once
 #include <string>
 #include <sstream>
 #include <vector>
 #include <math.h>
-namespace Cities {
-	using namespace std; 
+using namespace std; 
 
 
 
-	class city
-	{
-	private:
-		vector<double> cordinates;
-		string name;
-		string country;
+class city
+{
+private:
+	vector<double> cordinates;
+	string name;
+	string country;
 
-	public:
-		city();
-		~city();
-		//setter
-		void setCity(string location);
-		//getters
-		string getName();
-		string getCountry();
-		vector<double> getCordinates();
-		//distance
-		double distance(city& other);
+public:
+	city();
+	~city();
+	//setter
+	void setCity(string location);
+	//getters
+	string getName();
+	string getCountry();
+	vector<double> getCordinates();
+	//distance
+	double distance(city& other);
 		
-	};
-}
-#endif
+};
+
